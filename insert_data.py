82927bd73d2c9f15c5eeb6a308cb97887c86b348
Sadhana-rout-SR <routsadhana6@gmail.com>
2026-05-10 21:42:06 +0530

    Updated project files

---
+++ insert_data.py
@@ -0,0 +1,54 @@
+import sqlite3
+from werkzeug.security import generate_password_hash
+
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+
+# ADMIN
+cur.execute("""
+INSERT INTO users(name,email,password,role)
+VALUES(?,?,?,?)
+""", (
+    'Admin',
+    'admin@gmail.com',
+    generate_password_hash('admin123'),
+    'admin'
+))
+
+# EMPLOYEE
+cur.execute("""
+INSERT INTO users(name,email,password,role)
+VALUES(?,?,?,?)
+""", (
+    'Sadhana',
+    'employee@gmail.com',
+    generate_password_hash('employee123'),
+    'employee'
+))
+
+# PROJECT
+cur.execute("""
+INSERT INTO projects(title,description,created_by)
+VALUES(?,?,?)
+""", (
+    'Task Management System',
+    'Final Year Project',
+    1
+))
+
+# TASK
+cur.execute("""
+INSERT INTO tasks(title,description,status,assigned_to,project_id)
+VALUES(?,?,?,?,?)
+""", (
+    'Frontend Design',
+    'Create Dashboard UI',
+    'Pending',
+    2,
+    1
+))
+
+conn.commit()
+conn.close()
+
+print("Data Inserted Successfully")
